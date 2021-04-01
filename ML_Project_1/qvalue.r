b649f7fbf9b96ba052632e5032155bb50cebfde6
Your Name <you@example.com>
2021-04-01 17:59:25 -0600

    Updated qvalue.r on 2021-04-01. Finished script to obtain qvalues from pvalues and print the results.

--- ML_Project_1/qvalue.r
+++ ML_Project_1/qvalue.r
@@ -8,7 +8,7 @@ library('qvalue')
 # We read the options from the default: commandArgs(TRUE).
 spec = matrix(c(
     'infile','i', 1, "character",
-    'output_dir','o', 1, "character",
+    'snp_hwe_results_outfile','o', 1, "character",
     'help','h', 0, "logical"
 ), byrow=TRUE, ncol=4);
 opt = getopt(spec);
@@ -27,7 +27,7 @@ if(is.null(opt$infile)){
     cat(getopt(spec, usage=FALSE));
     q(status=1);
 }
-if(is.null(opt$output_dir)){
+if(is.null(opt$snp_hwe_results_outfile)){
     cat(getopt(spec, usage=FALSE));
     q(status=1);
 }
@@ -38,13 +38,57 @@ if(is.null(opt$output_dir)){
 infile = opt$infile;
 
 # The output directory parameter variable outfile_dir.
-output_dir = opt$output_dir;
+snp_hwe_results_outfile = opt$snp_hwe_results_outfile;
 
 # generate base output directory if it does not exist.
-dir.create(output_dir, showWarnings = FALSE);
+#dir.create(output_dir, showWarnings = FALSE);
 
 
 dataframe = read.table(infile, header=TRUE)
 
-head(dataframe)
+#head(dataframe)
+
+#print(dataframe[,3])
+qvalues = qvalue(p = dataframe[,3])
+
+print(qvalues)
+
+q_values = qvalues$qvalues
+
+#print(qvalues$lfdr)
+
+#summary(qvalues)
+#print(qvalues$qvalues)
+#head(qvalues)
+
+# Get the number of markers in the genotype counts dataframe.
+n_markers = nrow(dataframe)
+
+# Write the header of the snp_hwe_results_outfile file.
+cat(paste('marker_id', 'beta', 'p_value', 'q_value', sep='\t'), file=snp_hwe_results_outfile, sep='\n')
+#cat(paste('MARKER_ID', 'HET', 'HOM_1', 'HOM_2', 'p_value', sep='\t'), file=snp_hwe_results_outfile, sep='\n')
+
+# Iterate over the genotype counts dataframe.
+for (i in 1:n_markers) {
+
+    # Get the marker id at column 1.
+    marker_id  = dataframe[i, 1]
+
+    # Get the major allele frequency (p) at column 2.
+    beta = dataframe[i, 2]
+
+    # Get the minor allele frequency (q) at column 3.
+    p_value = dataframe[i, 3]
+
+    # Perform the exact SNP test of Hardy-Weinberg Equilibrium (HWE) to obtain a P-value for the marker.
+    q_value = q_values[i]
+
+    # Write the marker entry and results of the SNPHWE function to the snp_hwe_results_outfile file.
+    cat(paste(marker_id,beta,p_value,q_value,sep='\t'),file=snp_hwe_results_outfile,sep='\n',append=TRUE)
+    #cat(paste(marker_id,hets,hom_1,hom_2,p_value,sep='\t'),file=snp_hwe_results_outfile,sep='\n',append=TRUE)
+}
+
+
+
+
 
