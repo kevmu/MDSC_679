fb3c2e731303c5bdebc1744d2be64fe9c42c191d
Your Name <you@example.com>
2021-04-06 16:55:58 -0600

    Updated apriori_test_dataset2_job_array.sh on 2021-04-06. Added a logs directory to keep the logs files.

--- ML_Project_2/test_datasets/test_dataset2/apriori_test_dataset2_job_array.sh
+++ ML_Project_2/test_datasets/test_dataset2/apriori_test_dataset2_job_array.sh
@@ -6,16 +6,16 @@
 #SBATCH --time=20:00:00
 #SBATCH --mem=38G
 #SBATCH --array=1-10%10
-#SBATCH --output=logs/apriori_test_dataset2_job_array.%A_%a.out
-#SBATCH --error=logs/apriori_test_dataset2_job_array.%A_%a.err
+#SBATCH --output=apriori_test_dataset2_job_array.%A_%a.out
+#SBATCH --error=apriori_test_dataset2_job_array.%A_%a.err
 
 # The start time to print.
 start_time=$(date)
 echo "started at: ${start_time}"
 
 # Make a logs directory for the logs files if it does not already exist.
-logs_dir="${HOME}/MDSC_679/ML_Project_2/test_datasets/test_dataset2/logs"
-mkdir -p $logs_dir
+#logs_dir="${HOME}/MDSC_679/ML_Project_2/test_datasets/test_dataset2/logs"
+#mkdir -p $logs_dir
 
 # The list of apriori_genotype_pattern_files 
 list="${HOME}/MDSC_679/ML_Project_2/test_datasets/test_dataset2/test_dataset2_file_list.txt"
