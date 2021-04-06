b075c4468e9355819cae63ae3eb04289eca9d928
Your Name <you@example.com>
2021-04-06 12:39:17 -0600

    Add the real_genomic_dataset folder with files for running the Apriori algorithm on the genomic dataset from the first machine learning project on 2021-04-06.

---
+++ ML_Project_2/real_genomic_dataset/apriori_algorithm_job_array.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --partition=cpu2019
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=20:00:00
+#SBATCH --mem=38G
+#SBATCH --array=1-4%4
+#SBATCH --output=apriori_algorithm_job_array.%A_%a.out
+#SBATCH --error=apriori_algorithm_job_array.%A_%a.err
+
+# The list of apriori_genotype_pattern_files 
+list="/home/kevin.muirhead/MDSC_679/ML_Project_2/real_genomic_dataset/apriori_genotype_pattern_file_list.txt"
+
+output_dir="/home/kevin.muirhead/MDSC_679/ML_Project_2/real_genomic_dataset"
+mkdir -p $output_dir
+
+
+IFS=$'\n' array=($(<$list))
+
+# The apriori_genotype_pattern_file.
+apriori_genotype_pattern_file=${array[$SLURM_ARRAY_TASK_ID-1]}
+
+
